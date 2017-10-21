373d44404016d94b56596781d10d16d3f5450583
Nick Navarro <jacinto802@gmail.com>
2017-10-21 01:09:24 -0400

    Jquery, CSS, HTML updates

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,18 +6,52 @@ function pacingFunction(){
   console.log(hiddenPaceDiv);
 if($("#hiddenPaceDiv").val() >= 0 ){
   console.log('should be positive');
-  $("#yayOrNay").css("border-color", "green");
-  $("#yayOrNay").css("border-color", "green");
+  $("#yayOrNay").css("border-color", "rgba(115, 181, 101, 1)");
+  $("#yayOrNay").css("background-color", "rgba(115, 181, 101, .4)");
+  $("#yayOrNay").css("color", "white");
+  $("#yayOrNay").append("Sweet! You have an extra "+hiddenPaceDiv+" unused miles.");
 
 } else if ($("#hiddenPaceDiv").val() < 0){
       console.log('should be negative');
-      $("#yayOrNay").css("background-color", "red");
+      $("#yayOrNay").css("border-color", "rgba(224, 80, 80, 1)");
+      $("#yayOrNay").css("background-color", "rgba(224, 80, 80, .4)");
+      $("#yayOrNay").css("color", "white");
+      $("#yayOrNay").append("Yikes! Your balance is negative ("+hiddenPaceDiv+") miles.");
+
     };
 };
 pacingFunction();
 
+// $("#yayOrNay").append("Great Job You have an extra ",$("#hiddenPaceDiv").val() " of unused miles.");
 
+// $(document).ready(function(){
+//
+//
+// function pacingFunction(){
+//   const hiddenPaceDiv = $("#hiddenPaceDiv").val()
+//   console.log(hiddenPaceDiv);
+// if($("#hiddenPaceDiv").val() >= 0 ){
+//   console.log('should be positive');
+//   $("#fuck").css("border-color", "rgb(115, 181, 101)");
+//   $("#fuck").css("background-color-color", "rgb(115, 181, 101), .4");
+//
+// } else if ($("#hiddenPaceDiv").val() < 0){
+//       console.log('should be negative');
+//       $("#yayOrNay").css("background-color", "red");
+//     };
+// };
+// pacingFunction();
+//
+//
+//
 
+//
+//
+//
+//
+//
+// })
+//
 
 
 
